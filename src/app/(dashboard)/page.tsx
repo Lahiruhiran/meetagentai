3d542eb5e4ac19f36374c85bca05bd03e720afbf
lahiruhiran <lahiruhiranwijesooriyansbm@gmail.com>
2025-06-01 14:18:44 +0530

    social authentication 2

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,21 @@
+import HomeView from "@/modules/auth/ui/views/home-view";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+
+
+const Page = async () => {
+
+  const session = await auth.api.getSession({
+    headers : await headers(),
+  });
+  if (!session) {
+      redirect("/sign-in");
+  }
+
+  return <HomeView/>
+
+}
+export default Page;
+   
+  
--- src/modules/auth/ui/views/home-view.tsx
+++ src/modules/auth/ui/views/home-view.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 
 const Page = () => {
 
-  const { data: session } = authClient.useSession();
+    const { data: session } = authClient.useSession();
+    const router = useRouter();
   
   if (!session) {
     return (
@@ -16,7 +18,17 @@ const Page = () => {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
         <p className="text-2xl">You are already logged in as {session.user.name}</p>
-        <Button onClick={() => authClient.signOut()}>Sign Out</Button>
+            <Button onClick={() => authClient.signOut({
+                fetchOptions: {
+                    onSuccess: () => {
+                        router.push("/sign-in");
+                    }   
+                }
+            })
+            
+            }>
+                Sign Out
+            </Button>
       </div>
     );
   }
